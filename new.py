01ceca244275a1d75aa7b8b84eb757bac183d096
kbaikov <konstantin.baikov@gmail.com>
2024-09-15 23:13:53 +0200

    Add new file template for future solutions

---
+++ new.py
@@ -0,0 +1,34 @@
+"""based on https://github.com/nedbat/adventofcode2023/blob/main/new.py"""
+
+import re
+from dataclasses import dataclass
+
+TEST_INPUT = """\
+"""
+
+
+# def part1(lines):
+#     ...
+#
+#
+# def test_part1():
+#     assert part1(TEST_INPUT) == 123456
+#
+#
+# if __name__ == "__main__":
+#     answer = part1(file_lines("dayX_input.txt"))
+#     print(f"Part 1: {answer = }")
+
+
+# def part2(lines):
+#     ...
+#
+#
+# def test_part2():
+#     assert part2(TEST_INPUT) == 123456
+#
+#
+#
+# if __name__ == "__main__":
+#     answer = part2(file_lines("dayX_input.txt"))
+#     print(f"Part 2: {answer = }")
