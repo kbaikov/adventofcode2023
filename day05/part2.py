9569aff7ef71cfbf3e3638307922b8f3d43a6e86
kbaikov <konstantin.baikov@gmail.com>
2024-09-18 01:00:50 +0200

    Solve day05 crazy part2

--- day05/part2.py
+++ day05/part2.py
@@ -1,98 +1,13 @@
-import argparse
-import itertools
-from functools import partial
+"""based on https://github.com/norvig/pytudes/blob/main/ipynb/Advent-2023.ipynb"""
+
+from dataclasses import dataclass
+from itertools import batched
 from pathlib import Path
+from typing import Collection
 
-import pytest
+INPUT_TXT = Path("day05/input.txt")
 
-INPUT_TXT = Path("input.txt")
-
-
-def source_to_destination_list(value: int, destinations: list[range], sources: list[range]) -> int:
-    for i, s in enumerate(sources):
-        try:
-            return destinations[i][s.index(value)]
-        except ValueError:
-            continue
-    return value
-
-
-seed_soil = partial(
-    source_to_destination_list,
-    destinations=[range(50, 50 + 2), range(52, 52 + 48)],
-    sources=[range(98, 98 + 2), range(50, 50 + 48)],
-)
-
-soil_fertilizer = partial(
-    source_to_destination_list,
-    destinations=[range(0, 37), range(37, 2), range(39, 15)],
-    sources=[range(15, 37), range(52, 2), range(0, 15)],
-)
-
-
-def parse_table(s: str) -> dict[str, list[int]]:
-    d: dict[str, list[int]] = {}
-    for subdict in s.split("\n\n"):
-        key, value = subdict.split(":")
-        d[key] = value.strip().split("\n")
-
-    for key, value in d.copy().items():
-        if len(value) > 1:
-            d[key] = [[int(num) for num in string.split()] for string in value]
-        else:
-            d[key] = [int(num) for num in value[0].split()]
-    return d
-
-
-def list_to_partial(table: list[list[int]]):
-    destinations = []
-    sources = []
-    for entry in table:
-        destination_start, source_start, step = entry
-        destinations.append(range(destination_start, destination_start + step))
-        sources.append(range(source_start, source_start + step))
-    return partial(
-        source_to_destination_list,
-        destinations=destinations,
-        sources=sources,
-    )
-
-
-def seed_location(seed_number: int, d_maps):
-    d_funcs = {key: list_to_partial(value) for key, value in d_maps.items() if key != "seeds"}
-
-    return d_funcs["humidity-to-location map"](
-        d_funcs["temperature-to-humidity map"](
-            d_funcs["light-to-temperature map"](
-                d_funcs["water-to-light map"](
-                    d_funcs["fertilizer-to-water map"](
-                        d_funcs["soil-to-fertilizer map"](d_funcs["seed-to-soil map"](seed_number))
-                    )
-                )
-            )
-        )
-    )
-
-
-def compute(s: str) -> int:
-    d_maps = parse_table(s)
-
-    seed_ranges = []
-    for start, quantity in itertools.batched(d_maps["seeds"], n=2):
-        seed_ranges.append(range(start, start + quantity))
-
-    def all_seeds(a):
-        for seed_range in a:
-            for seed in seed_range:
-                yield seed
-
-    current_min = 999999999999999
-    for seed_number in all_seeds(seed_ranges):
-        current_min = min(seed_location(seed_number, d_maps), current_min)
-    return seed_locations
-
-
-INPUT_S = """\
+TEST_INPUT = """\
 seeds: 79 14 55 13
 
 seed-to-soil map:
@@ -127,27 +42,119 @@ humidity-to-location map:
 60 56 37
 56 93 4
 """
-EXPECTED = 46
 
 
-@pytest.mark.parametrize(
-    ("input_s", "expected"),
-    ((INPUT_S, EXPECTED),),
-)
-def test(input_s: str, expected: int) -> None:
-    assert compute(input_s) == expected
+@dataclass
+class Mapping:
+    range: range
+    offset: int
 
 
-def main() -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
-    args = parser.parse_args()
+def parse_almanac(paragraph) -> tuple:
+    """Parse a paragraph which can be either a list of seeds or a map."""
+    if paragraph.startswith("seeds:"):
+        name, *seed_numbers = paragraph.split()
+        return ("seeds", [int(seed) for seed in seed_numbers])
+    else:
+        name, *mappings = paragraph.splitlines()
+        int_mappings = [
+            list(map(int, entry)) for entry in [mapping.split() for mapping in mappings]
+        ]
+        final_mappings = []
+        for entry in int_mappings:
+            dest, src, length = entry
+            final_mappings.append(Mapping(range(src, src + length), dest - src))
 
-    with open(args.data_file, encoding="utf-8") as f:
-        print(compute(f.read()))
+        return (name, final_mappings)
 
-    return 0
+
+def parse_input(text):
+    paragraphs = text.split("\n\n")
+    return map(parse_almanac, paragraphs)
+
+
+def first(iterable, default=None) -> object | None:
+    """The first element in an iterable, or the default if iterable is empty."""
+    return next(iter(iterable), default)
+
+
+def convert(number: int, mappings: list[Mapping]) -> int:
+    """Convert number if it is contained in one of the range mappings; else leave unchanged."""
+    return first(number + m.offset for m in mappings if number in m.range) or number
+
+
+def multi_convert(number: int, almanac):
+    """Convert number using successive range mappings in almanac (but not 'seeds')."""
+    for name, mappings in almanac:
+        if name != "seeds":
+            number = convert(number, mappings)
+    return number
+
+
+def lowest_location(almanac):
+    """What is the lowest location number that corresponds to one of the seed numbers in almanac?"""
+    name, seeds = almanac[0]
+    return min(multi_convert(seed, almanac) for seed in seeds)
+
+
+def part2(text):
+    almanac = list(parse_input(text))
+    return lowest_location_with_ranges(almanac)
+
+
+def convert_ranges(ranges: Collection[range], mappings: list[Mapping]) -> set[range]:
+    """Convert a set of ranges into another set of ranges, as specified by the mappings."""
+    ranges = set(ranges)  # Make `ranges` be a set if it is not already
+    result = set()  # This will be the output set of ranges
+    while ranges:
+        r = ranges.pop()
+        m = find_intersecting_mapping(r, mappings)
+        if m:
+            start, stop = max(r.start, m.range.start), min(r.stop, m.range.stop)
+            result.add(range(start + m.offset, stop + m.offset))
+            if r.start < start:
+                ranges.add(range(r.start, start))
+            if stop < r.stop:
+                ranges.add(range(stop, r.stop))
+        else:
+            result.add(r)
+    return result
+
+
+def multi_convert_ranges(ranges: Collection[range], almanac):
+    """Convert ranges using all the range mappings in almanac (except 'seeds') successively."""
+    num_seeds = sum(map(len, ranges))
+    for name, mappings in almanac:
+        if name != "seeds":
+            ranges = list(convert_ranges(ranges, mappings))
+            assert (
+                sum(map(len, ranges)) == num_seeds
+            ), f"was {num_seeds} seeds; now {sum(map(len, ranges))}"
+    return ranges
+
+
+def lowest_location_with_ranges(almanac):
+    """What is the lowest location number that corresponds to one of the seed numbers in almanac?"""
+    name, pairs = almanac[0]
+    ranges = {range(start, start + length) for (start, length) in batched(pairs, 2)}
+    converted_ranges = multi_convert_ranges(ranges, almanac)
+    return min(r.start for r in converted_ranges)
+
+
+def find_intersecting_mapping(r: range, mappings) -> Mapping | None:
+    """If there is a mapping that intersects with range r, return it."""
+    return first(m for m in mappings if (m.range.start in r) or (r.start in m.range))
+
+
+test_maps = [Mapping(range(15, 5), offset=2), Mapping(range(5, 10), offset=100)]
+assert find_intersecting_mapping(range(1, 10), test_maps) == Mapping(range(5, 10), offset=100)
+assert set(convert_ranges({range(1, 10)}, test_maps)) == {range(1, 5), range(105, 110)}
+
+
+def test_part2():
+    assert part2(TEST_INPUT) == 46
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    answer = part2(INPUT_TXT.read_text())
+    print(f"Part 2: {answer = }")
