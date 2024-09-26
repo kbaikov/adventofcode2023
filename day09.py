74d6e71ce7d7def7f7519adafdec999e657e56b3
kbaikov <konstantin.baikov@gmail.com>
2024-09-26 20:35:03 +0200

    Solve day09 part2

--- day09.py
+++ day09.py
@@ -44,18 +44,35 @@ def test_extrapolate():
 if __name__ == "__main__":
     answer = part1(FILE)
     # answer = part1(TEST_INPUT)
+    # print(answer)
+
+
+def extrapolate_backwards(sequence: Sequence[int]) -> int:
+    s = sequence[:]
+    s = [y - x for x, y in zip(s, s[1:])]
+    if len(set(s)) == 1:
+        return sequence[0] - s[0]
+    else:
+        return sequence[0] - extrapolate_backwards(s)
+
+
+def test_extrapolate_backwards():
+    assert extrapolate_backwards([0, 3, 6, 9, 12, 15]) == -3
+    assert extrapolate_backwards([1, 3, 6, 10, 15, 21]) == 0
+    assert extrapolate_backwards([10, 13, 16, 21, 30, 45]) == 5
+
+
+def part2(text: str) -> int:
+    table = list(parse_table(text))
+
+    return sum(extrapolate_backwards(x) for x in table)
+
+
+def test_part2():
+    assert part2(TEST_INPUT) == 2
+
+
+if __name__ == "__main__":
+    answer = part2(FILE)
+    # answer = part2(TEST_INPUT)
     print(answer)
-
-
-# def part2(text: str)-> int:
-#     ...
-#
-#
-# def test_part2():
-#     assert part2(TEST_INPUT) == 123456
-#
-#
-#
-# if __name__ == "__main__":
-#     answer = part2(FILE)
-#     print(answer)
